7f6b1a8025964eab994a766fd6d432f7b3410b44
jjsenpai1 <ujjawal.sharma@preplaced.in>
2023-11-03 19:19:11 +0530

    setup1

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import './button.css';
+import React from "react";
 
 interface ButtonProps {
   /**
@@ -13,11 +12,12 @@ interface ButtonProps {
   /**
    * How large should the button be?
    */
-  size?: 'small' | 'medium' | 'large';
+  size?: "small" | "medium" | "large";
   /**
    * Button contents
    */
   label: string;
+  className?: string;
   /**
    * Optional click handler
    */
@@ -29,16 +29,24 @@ interface ButtonProps {
  */
 export const Button = ({
   primary = false,
-  size = 'medium',
+  size = "medium",
   backgroundColor,
   label,
+  className,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={[
+        "storybook-button",
+        `storybook-button--${size}`,
+        mode,
+        className,
+      ].join(" ")}
       style={{ backgroundColor }}
       {...props}
     >
